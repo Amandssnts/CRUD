4124dad90e6ed0d2447bd6c8639c19fd51d12887
AMANDA DE SOUSA SANTOS <amanda.ssantos151@senacsp.edu.br>
2025-04-10 13:53:42 -0300

    first commit

---
+++ cadastrodeclientes/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿
+namespace cadastrodeclientes
+{
+    partial class frmCadastrodeClientes
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelTopo = new System.Windows.Forms.Panel();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tbControlCadatro = new System.Windows.Forms.TabPage();
+            this.txtCPF = new System.Windows.Forms.MaskedTextBox();
+            this.lblCPF = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtNomeSocial = new System.Windows.Forms.TextBox();
+            this.txtNomeCompleto = new System.Windows.Forms.TextBox();
+            this.lblNomeSocial = new System.Windows.Forms.Label();
+            this.lblNomeCompleto = new System.Windows.Forms.Label();
+            this.tbConsulta = new System.Windows.Forms.TabPage();
+            this.lstCliente = new System.Windows.Forms.ListView();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.panelTopo.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tbControlCadatro.SuspendLayout();
+            this.tbConsulta.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelTopo
+            // 
+            this.panelTopo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.panelTopo.Controls.Add(this.lblTitulo);
+            this.panelTopo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTopo.ForeColor = System.Drawing.Color.Black;
+            this.panelTopo.Location = new System.Drawing.Point(0, 0);
+            this.panelTopo.Name = "panelTopo";
+            this.panelTopo.Size = new System.Drawing.Size(932, 100);
+            this.panelTopo.TabIndex = 0;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.White;
+            this.lblTitulo.Location = new System.Drawing.Point(32, 40);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(224, 25);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Cadastro de Clientes";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tbControlCadatro);
+            this.tabControl1.Controls.Add(this.tbConsulta);
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabControl1.Location = new System.Drawing.Point(33, 117);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(805, 288);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tbControlCadatro
+            // 
+            this.tbControlCadatro.Controls.Add(this.txtCPF);
+            this.tbControlCadatro.Controls.Add(this.lblCPF);
+            this.tbControlCadatro.Controls.Add(this.txtEmail);
+            this.tbControlCadatro.Controls.Add(this.lblEmail);
+            this.tbControlCadatro.Controls.Add(this.txtNomeSocial);
+            this.tbControlCadatro.Controls.Add(this.txtNomeCompleto);
+            this.tbControlCadatro.Controls.Add(this.lblNomeSocial);
+            this.tbControlCadatro.Controls.Add(this.lblNomeCompleto);
+            this.tbControlCadatro.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbControlCadatro.Location = new System.Drawing.Point(4, 29);
+            this.tbControlCadatro.Name = "tbControlCadatro";
+            this.tbControlCadatro.Padding = new System.Windows.Forms.Padding(3);
+            this.tbControlCadatro.Size = new System.Drawing.Size(797, 255);
+            this.tbControlCadatro.TabIndex = 0;
+            this.tbControlCadatro.Text = "Dados de Clientes";
+            this.tbControlCadatro.UseVisualStyleBackColor = true;
+            // 
+            // txtCPF
+            // 
+            this.txtCPF.Location = new System.Drawing.Point(181, 165);
+            this.txtCPF.Mask = "###,###,###-##";
+            this.txtCPF.Name = "txtCPF";
+            this.txtCPF.Size = new System.Drawing.Size(131, 26);
+            this.txtCPF.TabIndex = 7;
+            // 
+            // lblCPF
+            // 
+            this.lblCPF.AutoSize = true;
+            this.lblCPF.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCPF.ForeColor = System.Drawing.Color.Black;
+            this.lblCPF.Location = new System.Drawing.Point(37, 168);
+            this.lblCPF.Name = "lblCPF";
+            this.lblCPF.Size = new System.Drawing.Size(48, 20);
+            this.lblCPF.TabIndex = 6;
+            this.lblCPF.Text = "CPF:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(181, 119);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(313, 26);
+            this.txtEmail.TabIndex = 5;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.ForeColor = System.Drawing.Color.Black;
+            this.lblEmail.Location = new System.Drawing.Point(37, 122);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(64, 20);
+            this.lblEmail.TabIndex = 4;
+            this.lblEmail.Text = "E-mail:";
+            // 
+            // txtNomeSocial
+            // 
+            this.txtNomeSocial.Location = new System.Drawing.Point(181, 73);
+            this.txtNomeSocial.Name = "txtNomeSocial";
+            this.txtNomeSocial.Size = new System.Drawing.Size(313, 26);
+            this.txtNomeSocial.TabIndex = 3;
+            // 
+            // txtNomeCompleto
+            // 
+            this.txtNomeCompleto.Location = new System.Drawing.Point(181, 27);
+            this.txtNomeCompleto.Name = "txtNomeCompleto";
+            this.txtNomeCompleto.Size = new System.Drawing.Size(313, 26);
+            this.txtNomeCompleto.TabIndex = 2;
+            // 
+            // lblNomeSocial
+            // 
+            this.lblNomeSocial.AutoSize = true;
+            this.lblNomeSocial.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNomeSocial.ForeColor = System.Drawing.Color.Black;
+            this.lblNomeSocial.Location = new System.Drawing.Point(37, 76);
+            this.lblNomeSocial.Name = "lblNomeSocial";
+            this.lblNomeSocial.Size = new System.Drawing.Size(114, 20);
+            this.lblNomeSocial.TabIndex = 1;
+            this.lblNomeSocial.Text = "Nome Social:";
+            // 
+            // lblNomeCompleto
+            // 
+            this.lblNomeCompleto.AutoSize = true;
+            this.lblNomeCompleto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNomeCompleto.ForeColor = System.Drawing.Color.Black;
+            this.lblNomeCompleto.Location = new System.Drawing.Point(34, 30);
+            this.lblNomeCompleto.Name = "lblNomeCompleto";
+            this.lblNomeCompleto.Size = new System.Drawing.Size(141, 20);
+            this.lblNomeCompleto.TabIndex = 0;
+            this.lblNomeCompleto.Text = "Nome Completo:";
+            // 
+            // tbConsulta
+            // 
+            this.tbConsulta.Controls.Add(this.lstCliente);
+            this.tbConsulta.Controls.Add(this.btnBuscar);
+            this.tbConsulta.Controls.Add(this.txtBuscar);
+            this.tbConsulta.Location = new System.Drawing.Point(4, 29);
+            this.tbConsulta.Name = "tbConsulta";
+            this.tbConsulta.Padding = new System.Windows.Forms.Padding(3);
+            this.tbConsulta.Size = new System.Drawing.Size(797, 255);
+            this.tbConsulta.TabIndex = 1;
+            this.tbConsulta.Text = "Consulta";
+            this.tbConsulta.UseVisualStyleBackColor = true;
+            // 
+            // lstCliente
+            // 
+            this.lstCliente.HideSelection = false;
+            this.lstCliente.Location = new System.Drawing.Point(40, 77);
+            this.lstCliente.Name = "lstCliente";
+            this.lstCliente.Size = new System.Drawing.Size(693, 157);
+            this.lstCliente.TabIndex = 2;
+            this.lstCliente.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBuscar.ForeColor = System.Drawing.Color.White;
+            this.btnBuscar.Location = new System.Drawing.Point(591, 14);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(142, 42);
+            this.btnBuscar.TabIndex = 1;
+            this.btnBuscar.Text = "Pesquisar";
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(40, 22);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(545, 26);
+            this.txtBuscar.TabIndex = 0;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btnSalvar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSalvar.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalvar.ForeColor = System.Drawing.Color.White;
+            this.btnSalvar.Location = new System.Drawing.Point(654, 432);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(116, 42);
+            this.btnSalvar.TabIndex = 2;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = false;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // frmCadastrodeClientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(932, 500);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.panelTopo);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "frmCadastrodeClientes";
+            this.Text = "Cadastro";
+            this.panelTopo.ResumeLayout(false);
+            this.panelTopo.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tbControlCadatro.ResumeLayout(false);
+            this.tbControlCadatro.PerformLayout();
+            this.tbConsulta.ResumeLayout(false);
+            this.tbConsulta.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelTopo;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tbControlCadatro;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtNomeSocial;
+        private System.Windows.Forms.TextBox txtNomeCompleto;
+        private System.Windows.Forms.Label lblNomeSocial;
+        private System.Windows.Forms.Label lblNomeCompleto;
+        private System.Windows.Forms.TabPage tbConsulta;
+        private System.Windows.Forms.MaskedTextBox txtCPF;
+        private System.Windows.Forms.Label lblCPF;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.ListView lstCliente;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.TextBox txtBuscar;
+    }
+}
+
---
+++ cadastrodeclientes/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using MySql.Data.MySqlClient;
+
+
+namespace cadastrodeclientes
+{
+    public partial class frmCadastrodeClientes : Form
+    {
+        //Conexão com o banco de dados MySQL
+        MySqlConnection Conexao;
+        string data_source = "datasource=localhost; username=root; password=; database=cadastrodeclientes";
+
+        public frmCadastrodeClientes()
+        {
+            InitializeComponent();
+            //Condiguração inicial do ListView para exibição dos dados dos clientes.
+            lstCliente.View = View.Details; //Define a visualização como "Detalhes".
+            lstCliente.LabelEdit = true; //Permite ediatr os títulosdas colunas.
+            lstCliente.AllowColumnReorder = true; //Permite reordenar as colunas.
+            lstCliente.FullRowSelect = true; //Seleciona a linha inteira ao clicar.
+            lstCliente.GridLines = true; //Exibe as linhas de grade no ListView.
+
+            //Definindo as coluna no ListView.
+            lstCliente.Columns.Add("Codigo", 100, HorizontalAlignment.Left); //Coluna de código.
+            lstCliente.Columns.Add("Nome Completo", 200, HorizontalAlignment.Left); //Coluna de NomeCompleto.
+            lstCliente.Columns.Add("Nome Social", 200, HorizontalAlignment.Left); //Coluna de Nome Social.
+            lstCliente.Columns.Add("Email", 200, HorizontalAlignment.Left); //Coluna de E-mail.
+            lstCliente.Columns.Add("CPF", 200, HorizontalAlignment.Left); //Coluna de CPF.
+       
+
+            //Carrega os dados dos clientes na interface.
+            carregar_clientes();
+        }
+
+        private void carregar_clientes_com_query(string query)
+        {
+            try
+            {
+                //Criando a conexão com o banco de dados.
+                Conexao = new MySqlConnection(data_source);
+                Conexao.Open();
+
+                //Executa a consulta SQL fornecida.
+                MySqlCommand cmd = new MySqlCommand(query, Conexao);
+
+                //Se a consulta contém o parâmetro @q, adiciona o valor da caixa de pesquisa.
+                if (query.Contains("@q"))
+                {
+                    cmd.Parameters.AddWithValue("@q", "%" + txtBuscar.Text + "%");
+                }
+
+                //Executa o comando e obtém os resultados.
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                //Limpa os itens existentes no ListView antes de adicionar novos.
+                lstCliente.Items.Clear();
+
+                //Preenche o ListView com os dados dos clientes.
+                while (reader.Read())
+                {
+                    //Cria uma linha para cada cliente com os dados retornados da consulta.
+                    string[] row =
+                    {
+                    Convert.ToString(reader.GetInt32(0)), //Codigo
+                    reader.GetString(1),
+                    reader.GetString(2),
+                    reader.GetString(3),
+                    reader.GetString(4)
+                    };
+
+                    //Adiciona uma linha ao ListView
+                    lstCliente.Items.Add(new ListViewItem(row));
+                }
+            }
+
+            catch (MySqlException ex)
+            {
+                //Trata erros relacionados ao MySQL
+                MessageBox.Show("Erro " + ex.Number + " ocorreu: " + ex.Message,
+                    "Erro",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                //Trata outros tipos de erro * erros no backEnd do formulário
+                MessageBox.Show("Ocorreu: " + ex.Message,
+                    "Erro",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+
+            }
+            finally
+            {
+                //Garante que a conexão com o banco será fechada, mesmo se ocorrer erro
+                if (Conexao != null && Conexao.State == ConnectionState.Open)
+                {
+                    Conexao.Close();
+                }
+            }
+
+        }
+
+        //Método para carregar todos os clientes no ListView (usando uma consulta sem parâmetros).
+        private void carregar_clientes()
+        {
+            string query = "SELECT * FROM cadastro ORDER BY codigo DESC";
+            carregar_clientes_com_query(query);
+        }
+
+                    // Validação Regex - para avisar o usuário caso esqueça de preencher algum elemento doseu e-mail
+                    private bool isValidEmail(string email)
+                    {
+                        string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-z]{2,}$";
+                        Regex regex = new Regex(pattern);
+                        return regex.IsMatch(email);
+                    }
+                    // Função para validar se o cpf tem exatamente 11 digítos númericos.
+                    private bool isValidCPFLegth(string cpf)
+                    {
+                        // Remover quaisquer caracteres não númericos (como pontos e traços)
+                        cpf = cpf.Replace(".", "").Replace("-", "");
+                        // Verificar se o CPF tem exatamente 11 caracteres númericos
+                        if (cpf.Length != 11 || !cpf.All(char.IsDigit))
+                        {
+                            return false;
+                        }
+
+                        return true;
+                    }
+
+                    private void btnSalvar_Click(object sender, EventArgs e)
+                    {
+                        {
+                            try
+                            {
+                                // valiadeção de campos obrigátorios
+                                if (string.IsNullOrEmpty(txtNomeCompleto.Text.Trim()) ||
+                                   string.IsNullOrEmpty(txtEmail.Text.Trim()) || // o Trim ( .Trim) elimina o espaço vazio de um campo (Ex: gui fran) o espaço não entraria na contagem de caracteres. 
+                                    string.IsNullOrEmpty(txtCPF.Text.Trim()))
+
+                                {
+                                    MessageBox.Show("Todos os campos devem ser preenchidos.",
+                                        "Validação",
+                                        MessageBoxButtons.OK,
+                                        MessageBoxIcon.Warning);
+
+                                    return; //Impede o prosseguimento se algum campo estiver vazio
+                                }
+
+                                //validação do e-mail
+                                string email = txtEmail.Text.Trim();
+                                if (!isValidEmail(email))
+                                {
+                                    MessageBox.Show("Certifique-se de que o e-mail está no formato correto.",
+                                        "Validação",
+                                        MessageBoxButtons.OK,
+                                        MessageBoxIcon.Warning);
+                                    return; // impede o prosseguimento se o e-mail for inválido.
+                                }
+                                //Validadção do CPF
+                                string cpf = txtCPF.Text.Trim();
+                                if (!isValidCPFLegth(cpf))
+                                {
+                                    MessageBox.Show("CPF inválido. Certifique-se de que o CPF tenha 11 digítos númericos",
+                                         "Validação",
+                                        MessageBoxButtons.OK,
+                                        MessageBoxIcon.Warning);
+
+                                    return; //Impede o prosseguimento se algum campo estiver vazio
+                                }
+
+                                //Cria a conexão com o banco de dados
+                                Conexao = new MySqlConnection(data_source);
+                                Conexao.Open();
+
+                                //Teste de abertura de banco de dados
+                                // MessageBox.Show("Conexão aberta com sucesso");
+
+                                //Comando SQL para inserir um cliente no banco de dados
+
+                                MySqlCommand cmd = new MySqlCommand
+                                {
+                                    Connection = Conexao
+                                };
+
+                                cmd.Prepare();
+
+                                cmd.CommandText = "INSERT INTO cadastro(nomecompleto, nomesocial, email, cpf)" +
+                                    "VALUES (@nomecompleto, @nomesocial, @email, @cpf)";
+
+                            //Adiciona os parâmetros com os dados do formulário
+                                cmd.Parameters.AddWithValue("@nomecompleto", txtNomeCompleto.Text.Trim());
+                                cmd.Parameters.AddWithValue("@nomesocial", txtNomeSocial.Text.Trim());
+                                cmd.Parameters.AddWithValue("@email", email);
+                                cmd.Parameters.AddWithValue("@cpf", cpf);
+
+                                // Executa o comando de Inserção no banco
+                                cmd.ExecuteNonQuery();
+
+                                // mensagem de sucesso
+                                MessageBox.Show("Contato Inserido com sucesso: ",
+                                    "Sucesso",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Information);
+
+                    //Limpa os campos após o sucesso
+                    txtNomeCompleto.Text = String.Empty;
+                    txtNomeSocial.Text = "";
+                    txtEmail.Text = "";
+                    txtCPF.Text = "";
+
+                    //Recarregar os clientes na ListView.
+                    carregar_clientes();
+
+                    //Muda para a aba de pesquisa.
+                    tabControl1.SelectedIndex = 1;
+
+                            }
+                            catch (MySqlException ex)
+                            {
+                                //Trata erros relacionados ao MySQL
+                                MessageBox.Show("Erro " + ex.Number + " ocorreu: " + ex.Message,
+                                    "Erro",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Error);
+                            }
+                            catch (Exception ex)
+                            {
+                                //Trata outros tipos de erro * erros no backEnd do formulário
+                                MessageBox.Show("Ocorreu: " + ex.Message,
+                                    "Erro",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Error);
+
+                            }
+                            finally
+                            {
+                                //Garante que a conexão com o banco será fechada, mesmo se ocorrer erro
+                                if (Conexao != null && Conexao.State == ConnectionState.Open)
+                                {
+                                    Conexao.Close();
+
+                                    //Teste de fechamento do banco
+                                    //MessageBox.Show("Conexão fechada com sucesso");
+                                }
+                            }
+                        }
+
+                    }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT * FROM  cadastro WHERE nomecompleto LIKE @q OR nomesocial LIKE @q ORDER BY codigo DESC";
+            carregar_clientes_com_query(query);
+        }
+    }
+           
+}
+     
+
+
+
